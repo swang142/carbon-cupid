f3af9c3de2cf469faa5524aa108bc5cc9ef90fde
Kenny Guo <kennykguo@gmail.com>
2025-03-23 06:22:47 -0400

    update

--- server/app.js
+++ server/app.js
@@ -23,9 +23,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from uploads directory
-app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-
 // API routes
 app.use("/api/fundees", fundeeRoutes);
 app.use("/api/funders", funderRoutes);
